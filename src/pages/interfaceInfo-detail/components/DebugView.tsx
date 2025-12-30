853d5d149416ee335bb725fde8b004c5ce3973fb
杨建成 <11967785+yang-jiancheng@user.noreply.gitee.com>
2025-12-30 10:11:14 +0800

    SwaggerUI 文档展示

--- src/pages/interfaceInfo-detail/components/DebugView.tsx
+++ src/pages/interfaceInfo-detail/components/DebugView.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
-import {Button, Card, message} from "antd";
-import {debugInterfaceUsingPost} from "@/services/ant-design-pro/interfaceInfoController";
+import { debugInterfaceUsingPost } from "@/services/ant-design-pro/interfaceInfoController";
+import { Button, Card, message } from "antd";
+import React, { useState } from "react";
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
 
-const DebugView:React.FC = () => {
-
+const DebugView: React.FC = () => {
   const [result, setResult] = useState<string>("");
 
   const debug = async () => {
@@ -20,7 +21,19 @@ const DebugView:React.FC = () => {
   return (
     <Card>
       展示调用文档
-      <hr/>
+      <hr />
+      <SwaggerUI
+        url="/api/v2/api-docs?group=api"
+        requestInterceptor={(req: { url: string }) => {
+          // 把 http://localhost:8101/api/... 改成 /api/...
+          if (req.url?.startsWith("http://localhost:8101/api/api")) {
+            console.log('进去了。。。。', req.url)
+            req.url = req.url.replace("http://localhost:8101/api/api", "http://localhost:8101/api");
+            console.log('进去了。。。。', req.url)
+          }
+          return req;
+        }}
+      />
       <Button
         onClick={async () => {
           await debug();
@@ -30,7 +43,7 @@ const DebugView:React.FC = () => {
       </Button>
       <span>结果：{result}</span>
     </Card>
-  )
-}
+  );
+};
 
 export default DebugView;
