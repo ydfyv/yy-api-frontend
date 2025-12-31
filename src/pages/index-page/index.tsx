7ad0d1446bfb188c0ad41a14ebf0aa7080a30e7d
杨建成 <11967785+yang-jiancheng@user.noreply.gitee.com>
2025-12-31 16:03:01 +0800

    注册功能开发

--- src/pages/index-page/index.tsx
+++ src/pages/index-page/index.tsx
@@ -39,7 +39,6 @@ const indexPage: React.FC = () => {
         }}
         onDataSourceChange={setDataSource}
         pagination={{
-          current: 1,
           pageSize: 10,
         }}
         request={async (params) => {
